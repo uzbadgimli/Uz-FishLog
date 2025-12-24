7e3b1a2e11139388a71efa10bebd23994fd2874c
Uzbad <gsxr600f@gmail.com>
2025-12-24 16:51:17 +0300

    Statik export yapılandırması

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
-})
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  }
+}
 
-const nextConfig = {}
-
-module.exports = withPWA(nextConfig)
+module.exports = nextConfig
